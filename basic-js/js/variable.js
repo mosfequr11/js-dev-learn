6bca0020786ef28660162acce87f73a62e87c573
Md Mosfequr <mosfequr11@users.noreply.github.com>
2026-03-29 22:11:38 +0600

    learn: variable

--- basic-js/js/variable.js
+++ basic-js/js/variable.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+console.log("Asraful Developer");
+// number
+console.log("123456789");
+console.log("Asraful");
+
+let Mango = 20;
+let jackFruite = 15;
+let Orange = 25;
+
+let sum = Mango + Orange + jackFruite;
+console.log(sum);
